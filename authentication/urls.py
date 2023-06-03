a976b08db7cfac347766269f40fd13042b64775b
Mizanur Rahman <mizan.prog@gmail.com>
2023-06-04 00:01:27 +0600

    register

--- authentication/urls.py
+++ authentication/urls.py
@@ -1,6 +1,7 @@
-from .views import RegistrationViw
+from .views import RegistrationViw, UserNameValidationView
 from django.urls import path
-
+from django.views.decorators.csrf import csrf_exempt
 urlpatterns = [
-    path('register', RegistrationViw.as_view(), name='register')
+    path('register', RegistrationViw.as_view(), name='register'),
+    path('username-validation', csrf_exempt(UserNameValidationView.as_view()), name='user-name-validation')
 ]
--- authentication/views.py
+++ authentication/views.py
@@ -1,8 +1,22 @@
 from django.shortcuts import render
 from django.views import View
+import json
+from django.http import JsonResponse
+from django.contrib.auth.models import User
 
 
 # Create your views here.
+class UserNameValidationView(View):
+    def post(self, request):
+        data = json.loads(request.body)
+        username = data['username']
+        if not str(username).isalnum():
+            return JsonResponse({'username_error': 'username only should be alpha numeric'}, status=400)
+        if User.objects.filter(username=username).exists():
+            return JsonResponse({'username_error': 'username already added'}, status=400)
+        return JsonResponse({'username_valid': True})
+
+
 class RegistrationViw(View):
     def get(self, request):
         return render(request, 'authentication/registration.html')
